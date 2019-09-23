769f77fe26690a4315e469c0e88f05bbb7c72c2a
Summer Banister <sgbanister@gmail.com>
2019-09-23 14:18:34 -0700

    edited project 3

---
+++ Project 3/Project 3.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <ostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{   
+    cout << "Please input file name here: " << endl;
+    string file_name;
+    cin >> file_name;
+                    
+    ifstream ppm_file;
+    ppm_file.open(file_name);
+    if(ppm_file.fail())
+    {
+        cout << "Can not open file.";
+    }
+    else
+    {
+        string line;
+        getline(ppm_file, line);
+        cout << "Image Format: " << line << endl;
+        
+        getline(ppm_file, line, ' ');
+        cout << "Width: " << line << endl;
+        
+        getline(ppm_file, line);
+        cout << "Height: " << line << endl;
+        
+        getline(ppm_file, line);
+        cout << "Max Pixel Value: " << line << endl;
+        
+        cout << "Data:" << endl;
+        while(getline(ppm_file, line))
+        {
+            cout << line << endl;
+        }
+        
+        /*vector<string> data;
+        while(getline(ppm_file, line, ' '))
+        {
+            data.push_back(line);
+        }
+        
+        for(int i=0; i%3=0; ++i)
+        {
+                
+        }*/
+    }
+    ppm_file.close();
+    
+    return 0;
+}
+
